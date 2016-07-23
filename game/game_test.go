a4eb8d1157c0bf1407edcadc0a3226e6d043c985
vikassry <vikassry@gmail.com>
2016-07-23 19:29:07 +0530

    cleaning code

--- game/game_test.go
+++ game/game_test.go
@@ -5,14 +5,9 @@ import (
 )
 
 func TestBlackJackGameShouldStart(t *testing.T) {
-	// cards := Cards{Card{"s", "1"}, Card{"s", "2"}}
-	// player := Player{cards}
-	// dealer := Dealer{cards}
-	// deck := Deck{}
-	game := NewGame() //{player, dealer, deck}
+	game := NewGame()
 	players_card := game.NumberOfCardsForPlayer()
 	dealers_card := game.NumberOfCardsForDealer()
-	// game.start()
 	if players_card != 2 {
 		t.Errorf("Expected number of cards to be 2 for player, got %d", players_card)
 	}
