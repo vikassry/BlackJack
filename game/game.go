c85e5619060eb30feba81b9520e1e35c0aa4f0c3
vikassry <vikassry@gmail.com>
2016-07-23 14:09:52 +0530

    Setup game test

---
+++ game/game.go
@@ -0,0 +1,19 @@
+package game
+
+// type Game struct {
+// 	player Player
+// 	dealer Dealer
+// 	deck Deck
+// }
+
+// type Player struct{
+
+// }
+
+// type Dealer struct{
+
+// }
+
+// type Deck struct{
+	
+// }
---
+++ game/game_test.go
@@ -0,0 +1,6 @@
+package game
+import "testing"
+
+func TestBlackJackGameShouldStart(t *testing.T){
+	// Game := Game{player, dealer, deck}
+}
