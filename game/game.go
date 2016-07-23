bbe9c6339fc5b440e5317d84590bc89c0ffb7364
vikassry <vikassry@gmail.com>
2016-07-23 14:44:29 +0530

    On starting of game player and dealer are assigned 2 cards

--- game/game.go
+++ game/game.go
@@ -1,19 +1,34 @@
 package game
 
-// type Game struct {
-// 	player Player
-// 	dealer Dealer
-// 	deck Deck
-// }
+type Game struct {
+	player Player
+	dealer Dealer
+	deck   Deck
+}
 
-// type Player struct{
+type Cards []Card
 
-// }
+type Card struct {
+	value string
+	group string
+}
 
-// type Dealer struct{
+type Player struct {
+	cards Cards
+}
 
-// }
+type Dealer struct {
+	cards Cards
+}
 
-// type Deck struct{
-	
-// }
+type Deck struct {
+	cards Cards
+}
+
+func (game Game) NumberOfCardsForPlayer() int {
+	return len(game.player.cards)
+}
+
+func (game Game) NumberOfCardsForDealer() int {
+	return len(game.dealer.cards)
+}
--- game/game_test.go
+++ game/game_test.go
@@ -1,6 +1,22 @@
 package game
-import "testing"
 
-func TestBlackJackGameShouldStart(t *testing.T){
-	// Game := Game{player, dealer, deck}
+import (
+	"testing"
+)
+
+func TestBlackJackGameShouldStart(t *testing.T) {
+	cards := Cards{Card{"s", "1"}, Card{"s", "2"}}
+	player := Player{cards}
+	dealer := Dealer{cards}
+	deck := Deck{}
+	game := Game{player, dealer, deck}
+	players_card := game.NumberOfCardsForPlayer()
+	dealers_card := game.NumberOfCardsForDealer()
+
+	if players_card != 2 {
+		t.Errorf("Expected number of cards to be 2 for player, got %d", players_card)
+	}
+	if dealers_card != 2 {
+		t.Errorf("Expected number of cards to be 2 for dealer, got %d", dealers_card)
+	}
 }
